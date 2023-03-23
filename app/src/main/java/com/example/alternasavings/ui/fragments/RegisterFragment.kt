7c97bd8a49adedd30f53b4202462ad36cfa4ba87
gichuhiii <gnwcocoe@gmail.com>
2023-03-23 11:23:12 +0300

    added fragments

--- app/src/main/java/com/example/alternasavings/ui/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/alternasavings/ui/fragments/RegisterFragment.kt
@@ -1,35 +1,20 @@
 package com.example.alternasavings.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import com.example.alternasavings.R
-import com.example.alternasavings.databinding.FragmentRegisterBinding
 
 
 class RegisterFragment : Fragment() {
-
-        private lateinit var binding: FragmentRegisterBinding
-
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            binding = FragmentRegisterBinding.inflate(inflater, container, false)
-            return binding.root
-        }
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            arguments?.let {
-
-            }
-        }
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
 }
+
---
+++ app/src/main/java/com/example/alternasavings/ui/viewmodels/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.alternasavings.ui.viewmodels
+
+class MainViewModel {
+
+}
