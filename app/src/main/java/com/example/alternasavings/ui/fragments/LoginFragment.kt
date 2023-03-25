1c714361d65358cc1b6276f3a5a19ed027e17077
gichuhiii <gnwcocoe@gmail.com>
2023-03-25 12:30:16 +0300

    nav_graph

--- app/src/main/java/com/example/alternasavings/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/alternasavings/ui/fragments/LoginFragment.kt
@@ -4,17 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.alternasavings.R
 
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        //inflate layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val button = view.findViewById<ConstraintLayout>(R.id.cl_next)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_OTPVerificationFragment)
+        }
+        return view
     }
 }
 
