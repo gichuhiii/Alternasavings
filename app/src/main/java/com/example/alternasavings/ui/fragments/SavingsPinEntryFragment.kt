25650ea7c4f6372b7445842c59dc42a40ce3b466
gichuhiii <gnwcocoe@gmail.com>
2023-03-25 12:20:14 +0300

    savings pin entry

--- app/src/main/java/com/example/alternasavings/ui/fragments/SavingsPinEntryFragment.kt
+++ app/src/main/java/com/example/alternasavings/ui/fragments/SavingsPinEntryFragment.kt
@@ -7,12 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.alternasavings.R
 
-class ActivationCodeEntryFragment : Fragment() {
+
+class SavingsPinEntryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_activation_code_entry, container, false)
+        return inflater.inflate(R.layout.fragment_savings_pin_entry, container, false)
     }
 }
