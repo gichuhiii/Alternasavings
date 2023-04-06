9f6a9160f619c368fe690023a62c4b962f71ccb7
gichuhiii <gnwcocoe@gmail.com>
2023-04-06 09:57:11 +0300

    autocomplete textview

--- app/src/main/java/com/example/alternasavings/ui/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/alternasavings/ui/fragments/RegisterFragment.kt
@@ -21,20 +21,15 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        //for auto complete text view
-        val autoCompleteTextView = view?.findViewById<AutoCompleteTextView>(R.id.autocomplete_textview)
-        val suggestions = arrayOf("Male", "Female", "Prefer not to say")
-
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, suggestions)
-        autoCompleteTextView?.setAdapter(adapter)
-        autoCompleteTextView?.setOnItemClickListener { parent, view, position, id ->
-            val selectedName = parent.getItemAtPosition(position) as String
-            // do something with the selected name here
-        }
-
-
         //inflate layout for this fragment
         val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        //for auto complete text view
+        val genderOptions = listOf("Male", "Female", "Prefer not to say")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, genderOptions)
+        val genderAutoCompleteTextView = view?.findViewById<AutoCompleteTextView>(R.id.autocomplete_textview)
+        genderAutoCompleteTextView?.setAdapter(adapter)
+
         val button = view.findViewById<ConstraintLayout>(R.id.cl_next)
         button.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_OTPVerificationFragment)
