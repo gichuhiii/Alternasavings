c3e195e2f41f327d72aa375d2c772118761c1133
gichuhiii <gnwcocoe@gmail.com>
2023-04-07 23:04:48 +0300

    Dashboard Fragment

--- app/src/main/java/com/example/alternasavings/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/example/alternasavings/ui/fragments/DashboardFragment.kt
@@ -13,6 +13,7 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
     }
